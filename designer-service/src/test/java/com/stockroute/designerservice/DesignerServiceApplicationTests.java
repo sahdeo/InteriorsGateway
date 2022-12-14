63a19b3a8048963a01b902b65e600b016ec23f7d
pemmasani.chinnaanji <pemmasani.chinnaanji@globallogic.com>
2022-12-14 16:41:39 +0530

    design service added

---
+++ designer-service/src/test/java/com/stockroute/designerservice/DesignerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockroute.designerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
