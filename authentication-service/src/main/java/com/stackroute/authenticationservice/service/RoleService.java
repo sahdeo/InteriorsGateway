76246924232eb747ec07d7df2b9f7200bc470542
mirazul <sk.haque@globallogic.com>
2022-12-19 13:52:41 +0530

    role entity, dao nd service added | running sucessfully

---
+++ authentication-service/src/main/java/com/stackroute/authenticationservice/service/RoleService.java
@@ -0,0 +1,17 @@
+package com.stackroute.authenticationservice.service;
+
+import com.stackroute.authenticationservice.dao.RoleDao;
+import com.stackroute.authenticationservice.entity.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService {
+
+    @Autowired
+    private RoleDao roleDao;
+
+    public Role createNewRole(Role role) {
+        return roleDao.save(role);
+    }
+}
