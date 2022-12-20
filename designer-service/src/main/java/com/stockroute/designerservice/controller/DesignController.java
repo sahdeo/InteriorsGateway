8df5970a5401f4b593520fc85593461222d79c3c
pemmasani.chinnaanji <pemmasani.chinnaanji@globallogic.com>
2022-12-20 19:37:15 +0530

    Design added

---
+++ designer-service/src/main/java/com/stockroute/designerservice/controller/DesignController.java
@@ -0,0 +1,62 @@
+package com.stockroute.designerservice.controller;
+import com.stockroute.designerservice.exception.DesignAlreadyExistsException;
+import com.stockroute.designerservice.exception.DesignNotFoundException;
+import com.stockroute.designerservice.model.Design;
+import com.stockroute.designerservice.service.DesignService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/designs/")
+public class DesignController {
+    private DesignService designService;
+
+    private ResponseEntity responseEntity;
+
+    @Autowired
+    public DesignController(DesignService designService) {
+        this.designService = designService;
+    }
+    @PostMapping("design")
+    public ResponseEntity<?> saveDesign(@RequestBody Design design) throws DesignAlreadyExistsException {
+        try {
+            designService.saveDesign(design);
+            responseEntity = new ResponseEntity<>(design, HttpStatus.CREATED);
+        } catch (DesignAlreadyExistsException e) {
+            throw new DesignAlreadyExistsException();
+        } catch (Exception e) {
+            responseEntity = new ResponseEntity<>("Error try save after sometime", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return responseEntity;
+
+    }
+    @GetMapping("design")
+    public ResponseEntity<?> getDesignDetails() {
+        try {
+            List<Design> designList = designService.getDesignDetails();
+            responseEntity = new ResponseEntity<>(designList, HttpStatus.OK);
+        } catch (Exception e) {
+            responseEntity = new ResponseEntity<>("Error try save after sometime", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return responseEntity;
+    }
+    @DeleteMapping("design/{designId}")
+    public ResponseEntity<?> deleteDesign(@PathVariable String designId) throws DesignNotFoundException {
+        try {
+            designService.deleteDesign(designId);
+            responseEntity = new ResponseEntity<>("Successfully deleted!!!!", HttpStatus.OK);
+        } catch (DesignNotFoundException e) {
+            throw new DesignNotFoundException();
+        } catch (Exception e) {
+            responseEntity = new ResponseEntity<>("Error try after sometime", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return responseEntity;
+
+    }
+
+}
--- designer-service/src/main/java/com/stockroute/designerservice/repository/DesignRepository.java
+++ designer-service/src/main/java/com/stockroute/designerservice/repository/DesignRepository.java
@@ -1,4 +1,13 @@
 package com.stockroute.designerservice.repository;
 
-public interface DesignRepository {
+import com.stockroute.designerservice.model.Design;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface DesignRepository extends MongoRepository<Design,String> {
+    @Query ("{'designDetails.designCode':{$in:[?0]}}")
+    List<Design> findAllDesignFromCode(int designCode);
 }
