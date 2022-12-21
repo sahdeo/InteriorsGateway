6214e603ff61061163d0e0292d24fdc71ca1ea4e
sarbeswar.pal <sarbeswar.pal@globallogic.com>
2022-12-21 14:03:34 +0530

    added lombok

--- email-service/src/main/java/com/stackroute/emailservice/model/EmailRequest.java
+++ email-service/src/main/java/com/stackroute/emailservice/model/EmailRequest.java
@@ -1,19 +1,15 @@
 package com.stackroute.emailservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmailRequest {
     private String toEmail;
     private String emailSubject;
     private String emailBody;
 
-    public EmailRequest() {
-
-    }
-
-    public EmailRequest(String toEmail, String emailSubject, String emailBody) {
-        this.toEmail = toEmail;
-        this.emailSubject = emailSubject;
-        this.emailBody = emailBody;
-    }
 
     public String getToEmail() {
         return toEmail;
