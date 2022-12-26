3bdeeddd5dfaf9f187a3ca63b7c9fe8e4193d758
mirazul <sk.haque@globallogic.com>
2022-12-27 00:21:04 +0530

    producer added

--- authentication-service/src/main/java/com/stackroute/authenticationservice/rabbitmqConfig/consumer.java
+++ authentication-service/src/main/java/com/stackroute/authenticationservice/rabbitmqConfig/consumer.java
@@ -2,10 +2,18 @@ package com.stackroute.authenticationservice.rabbitmqConfig;
 
 import com.stackroute.authenticationservice.dto.JwtRequest;
 import com.stackroute.authenticationservice.entity.User;
+import com.stackroute.authenticationservice.service.IUserService;
+import com.stackroute.authenticationservice.service.UserServiceImp;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import rabbitmq.domain.UserDto;
 
+@Component
 public class consumer {
-
+    @Autowired
+    private IUserService userService;
+    @RabbitListener(queues = "user_queue")
     public void getDataFromRabbitmq(UserDto userDto){
         User user = new User();
         user.setUserName(userDto.getUserName());
@@ -13,5 +21,6 @@ public class consumer {
         user.setUserLastName(userDto.getUserLastName());
         user.setUserPassword(userDto.getUserPassword());
         user.setRole(userDto.getRole());
+        userService.registerNewUser(user);
     }
 }
