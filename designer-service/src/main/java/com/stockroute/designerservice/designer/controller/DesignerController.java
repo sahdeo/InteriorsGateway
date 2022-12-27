6adb91f41e835b628bf570eed30bda6388df3408
pemmasani.chinnaanji <pemmasani.chinnaanji@globallogic.com>
2022-12-27 14:45:44 +0530

    designer-service test cases completed

--- designer-service/src/main/java/com/stockroute/designerservice/designer/controller/DesignerController.java
+++ designer-service/src/main/java/com/stockroute/designerservice/designer/controller/DesignerController.java
@@ -1,70 +1,46 @@
 package com.stockroute.designerservice.designer.controller;
-import com.stockroute.designerservice.designer.exception.DesignerAlreadyExitsException;
-import com.stockroute.designerservice.designer.exception.DesignerNotFoundException;
-import com.stockroute.designerservice.designer.model.DesignerEntity;
+import com.stockroute.designerservice.designer.exception.ProfileAlreadyExit;
+import com.stockroute.designerservice.designer.model.Designer;
 import com.stockroute.designerservice.designer.service.DesignerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v11")
 public class DesignerController {
-    @Autowired
+
+
     private DesignerService designerService;
-    private ResponseEntity responseEntity;
-
-    @PostMapping("/addDesigner")
-    public ResponseEntity createDesignerProfile(@RequestBody DesignerEntity designerEntity)  throws DesignerAlreadyExitsException{
-        try {
-            designerService.saveDesigner(designerEntity);
-            responseEntity = new ResponseEntity<>(designerEntity, HttpStatus.CREATED);
-        } catch (DesignerAlreadyExitsException e) {
-            throw new DesignerAlreadyExitsException();
-        } catch (Exception e) {
-            responseEntity = new ResponseEntity<>("Error try save after sometime", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-        return responseEntity;
+    @Autowired
+    public DesignerController(DesignerService designerService) {
+        this.designerService = designerService;
     }
 
-    @GetMapping ("/AllUDesigners")
+    @PostMapping("/addDesigner")
+    public Designer createDesignerProfile(@RequestBody Designer designer) throws ProfileAlreadyExit {
+        return designerService.saveDesigner(designer);
+    }
+
+    @GetMapping("/AllUDesigners")
     public ResponseEntity<?> getAllDesignerDetails() {
         return new ResponseEntity<>(designerService.getAllDesigners(), HttpStatus.OK);
     }
 
-    @GetMapping("/findByDesignerId/{designerId}")
-    public ResponseEntity<?> getUserByDesignerId(@PathVariable String designerId) {
-        return new ResponseEntity<>(designerService.findDesignersByDesignerId(designerId), HttpStatus.OK);
-    }
-    @GetMapping("/findByEmailId/{emailId}")
-    public ResponseEntity<?> getUserByEmailId(@PathVariable String emailId) {
-        return new ResponseEntity<>(designerService.findDesignersByEmailId(emailId), HttpStatus.OK);
+    @GetMapping("/findByEmailId/{EmailId}")
+    public ResponseEntity<?> getdesignerByEmailId(@PathVariable String EmailId) {
+        return new ResponseEntity<>(designerService.findDesignersByEmailId(EmailId), HttpStatus.OK);
     }
 
-    @DeleteMapping("/deletebyDesignerId/{designerId}")
-    public ResponseEntity<?> deleteUser(@PathVariable("designerId") String designerId)throws DesignerNotFoundException{
-        try {
-            designerService.deleteDesignerById(designerId);
-            responseEntity = new ResponseEntity<>("Successfully deleted!!!!", HttpStatus.OK);
-        } catch (DesignerNotFoundException e) {
-            throw new DesignerNotFoundException();
-        } catch (Exception e) {
-            responseEntity = new ResponseEntity<>("Error try after sometime", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return responseEntity;
-
+    @DeleteMapping("/deletebyEmail/{designerId}")
+    public ResponseEntity<?> deleteUser(@PathVariable("designerId") String designerId){
+        return new ResponseEntity<>(designerService.deleteDesignerById(designerId), HttpStatus.ACCEPTED);
     }
-
-
 
     @DeleteMapping("/clearAllHistory")
     public ResponseEntity<String> clearOrderHistory(){
-        return new ResponseEntity<>(designerService.deleteAllOrders(), HttpStatus.OK);
+        return new ResponseEntity<>(designerService.deleteAllOrders(),HttpStatus.OK);
     }
-
-
 
 }
