777fef76e6c15b92021a46d818a42774b066fd92
aravinth <a.sabareeswaran@globallogic.com>
2022-12-27 12:58:18 +0530

    eureka service dependency and annoations added in email service

--- email-service/src/main/java/com/stackroute/emailservice/EmailServiceApplication.java
+++ email-service/src/main/java/com/stackroute/emailservice/EmailServiceApplication.java
@@ -5,7 +5,9 @@ import com.stackroute.emailservice.service.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EmailServiceApplication {
 
