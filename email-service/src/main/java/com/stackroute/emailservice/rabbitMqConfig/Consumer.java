a89ff4554e13a90b4edc5dee450e83ebf0ca81e3
mirazul <sk.haque@globallogic.com>
2022-12-29 15:17:58 +0530

    forgot password added

---
+++ email-service/src/main/java/com/stackroute/emailservice/rabbitMqConfig/Consumer.java
@@ -0,0 +1,35 @@
+package com.stackroute.emailservice.rabbitMqConfig;
+
+import com.stackroute.authenticationservice.entity.User;
+import com.stackroute.authenticationservice.service.UserServiceImp;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import rabbitmq.domain.UserDto;
+
+@Component
+public class Consumer {
+    @Autowired
+    private UserServiceImp userService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @RabbitListener(queues="user-queue")
+    public void getDataFromRabbitmq(UserDto userDto) throws Exception{
+        User user = new User();
+        user.setEmailId(userDto.getEmailId());
+        user.setUserPassword(userDto.getUserPassword());
+        user.setConfirmPassword(userDto.getConfirmPassword());
+        user.setUserFirstName(userDto.getUserFirstName());
+        user.setUserLastName(userDto.getUserLastName());
+        user.setMobileNo(userDto.getMobileNo());
+        user.setRole(userDto.getRole());
+        userService.registerNewUser(user);
+    }
+
+    public String getEncodedPassword(String password) {
+       return new BCryptPasswordEncoder().encode(password);
+        //return passwordEncoder.de(password);
+    }
+}
---
+++ email-service/src/main/java/rabbit/domain/UserDto.java
@@ -0,0 +1,2 @@
+package rabbit.domain;public class UserDto {
+}
