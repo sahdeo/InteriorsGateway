ebc8cc98338bb64c88e690c07182a5ee729d85cb
aravinth <a.sabareeswaran@globallogic.com>
2023-01-02 15:37:26 +0530

    new updates

--- payment-service/src/main/java/com/stackroute/paymentservice/PaymentController.java
+++ payment-service/src/main/java/com/stackroute/paymentservice/PaymentController.java
@@ -2,13 +2,13 @@ package com.stackroute.paymentservice;
 
 
 import com.paytm.pg.merchant.PaytmChecksum;
-import com.stackroute.customerservice.paymentservice.PaytmDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
