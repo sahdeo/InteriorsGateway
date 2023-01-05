db1c5d4bac0f44e62f89267ba85aabd472570127
mirazul <sk.haque@globallogic.com>
2023-01-05 15:59:22 +0530

    docker file added in auth | api-gateway | customer | user | eureka | email

--- designer-service/src/test/java/com/stockroute/designerservice/DesignerServiceApplicationTests.java
+++ designer-service/src/test/java/com/stockroute/designerservice/DesignerServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.stockroute.designerservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DesignerServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.stockroute.designerservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DesignerServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
